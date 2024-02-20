f869e584da8319a06084363907883d70e26b360e
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:44:21 +0500

    project 34
    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
    • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    
    • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!

--- main.ts
+++ main.ts
@@ -1,32 +1,35 @@
-// 33: Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 
-// 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
-// • Store the numbers 1 through 9 in a array.
+// 34: Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza 
+// names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of
+//  printing just the name of the pizza. For each pizza you should have one line of output 
+//  containing a simple statement like I like pepperoni pizza.
 
-// • Loop through the array.
+// • Add a line at the end of your program, outside the for loop, that states how much you 
+// like pizza. The output should consist of three or more lines about the kinds of pizza you 
+// like and then an additional sentence, such as I really love pizza!
 
-// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. 
-// Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on 
-// a separate line.
+/*-------------------start working------------------*/
 
-/*----------------------start working------------------------------*/
+// Array containing favorite pizza names
+const pizzas: string[] = ["California Pizza", "Mexico Pizza", "Detroit-style Pizza."];
 
-// Store numbers 1 through 9 in an array
-const Ordinal_Numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Loop through the array
-for (const number of Ordinal_Numbers) {
-    // Use if-else chain to determine the proper ordinal ending
-    let ordinalEnding: string;
-    if (number === 1) {
-        ordinalEnding = "st";
-    } else if (number === 2) {
-        ordinalEnding = "nd";
-    } else if (number === 3) {
-        ordinalEnding = "rd";
-    } else {
-        ordinalEnding = "th";
-    }
-    // Print the number with its ordinal ending
-    console.log(`${number}${ordinalEnding}`);
+// Printing each pizza name
+console.log("Printing pizza names:");
+for (let pizza of pizzas) {
+    console.log(pizza);
 }
+
+// Printing a sentence for each pizza
+console.log("\nPrinting sentences about each pizza:");
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+// Printing additional statement about how much I like pizza
+console.log("\nI really love pizza!");
+
+
+
+
+
 
