fe9916534b98c1513bf821f5f66283330706b78c
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:38:53 +0500

    project28
    Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
    • If the person is less than 2 years old, print a message that the person is a baby.
    
    • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
    
    • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
    
    • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
    
    • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
    
    • If the person is age 65 or older, print a message that the person is an elder.

--- main.ts
+++ main.ts
@@ -1,95 +1,58 @@
-// 27: Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
-// • If the alien is green, print a message that the player earned 5 points.
+// 28: Stages of Life: Write an if-else chain that determines a person’s stage of life. 
+// Set a value for the variable age, and then:
+// • If the person is less than 2 years old, print a message that the person is a baby.
 
-// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the person is at least 2 years old but less than 4, print a message that the person 
+// is a toddler.
 
-// • If the alien is red, print a message that the player earned 15 points.
+// • If the person is at least 4 years old but less than 13, print a message that the person
+//  is a kid.
 
-// • Write three versions of this program, making sure each message is printed for the
-//  appropriate color alien.
+// • If the person is at least 13 years old but less than 20, print a message that the person 
+// is a teenager.
 
-/*----------------start work---------------*/
-/*------------1st work--------------*/
+// • If the person is at least 20 years old but less than 65, print a message that the person 
+// is an adult.
 
-// let Alien_Colors : string = "yellow"
+// • If the person is age 65 or older, print a message that the person is an elder.
 
-// if(Alien_Colors === "yellow"){
-//     console.log("player just earned 5 points")
+/*----------start work--------*/
+/*------------1st step------*/
+
+
+// let Age: number = 2
+// if (Age < 2){
+//     console.log("the person is a baby")
+
+// }else if (Age>=2 && Age <4){
+//     console.log("the person is a toddler")
+
+// }else if (Age>=4 && Age <13){
+//     console.log("the person is a kid.")
+ 
+// }else if (Age>=13 && Age <20){
+//     console.log("the person is a teenager.")
+
+// }else if (Age>=20 && Age <65){
+//     console.log("the person is a adult.")
+ 
+// }else if (Age>=65){
+//     console.log("the person is a elder")
 // }
-// else if(Alien_Colors === "green"){
-//     console.log("Player just earned 10 points")
 
-// }
-// else if(Alien_Colors === "red"){
-//    console.log("player just earned 5 points")
-// } 
+/*--------------2nd step ----------------*/
+let age: number = 25;
 
-
-/*-----------------2ns word_----------*/
-
-
-// let Alien_Colors : string = "red"
-// if(Alien_Colors === "yellow"){
-//     console.log("player just earned 5 points")
-// }
-// else if(Alien_Colors === "green"){
-//     console.log("Player just earned 10 points")
-
-// }
-// else if(Alien_Colors === "red"){
-//    console.log("player just earned 5 points")
-// } 
-
-
-
-/*------------3rd work-----*/
-// Green alien
-let alien_color1: string = 'green';
-
-if (alien_color1 === 'green') {
-    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
-} else if (alien_color1 === 'yellow') {
-    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
-} else if (alien_color1 === 'red') {
-    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+if (age < 2) {
+    console.log("The person is a baby.");
+} else if (age >= 2 && age < 4) {
+    console.log("The person is a toddler.");
+} else if (age >= 4 && age < 13) {
+    console.log("The person is a kid.");
+} else if (age >= 13 && age < 20) {
+    console.log("The person is a teenager.");
+} else if (age >= 20 && age < 65) {
+    console.log("The person is an adult.");
+} else {
+    console.log("The person is an elder.");
 }
-
-
-
-// Yellow alien
-let alien_color2: string = 'yellow';
-
-if (alien_color2 === 'green') {
-    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
-} else if (alien_color2 === 'yellow') {
-    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
-} else if (alien_color2 === 'red') {
-    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
-}
-
-
-
-
-// Red alien
-let alien_color3: string = 'red';
-
-if (alien_color3 === 'green') {
-    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
-} else if (alien_color3 === 'yellow') {
-    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
-} else if (alien_color3 === 'red') {
-    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
