f5d45779376fa83fcde96ea2610945275edb89a5
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:50:59 +0500

    project 42
    Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-// 41: Magicians: Make a array of magician’s names. Pass the array to a function 
-// called show_magicians(), which prints the name of each magician in the array.
-/*-----------------start working-------------*/
+// 42: Great Magicians: Start with a copy of your program from Exercise 
+// 39. Write a function called make_great() that modifies the array of magicians 
+// by adding the phrase the Great to each magician’s name. Call show_magicians() 
+// to see that the list has actually been modified.
+/*-----------start working----------*/
+// function show_magicians(magicians: string[]): void {
+//     magicians.forEach(magician => console.log(magician));
+// }
+// function make_great(magicians: string[]): void {
+//     for (let i = 0; i < magicians.length; i++) {
+//         magicians[i] = "the Great " + magicians[i];
+//      }
+// }
+// Array of magician's names
+// const magicianNames: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+// Calling make_great() to modify magician names
+// make_great(magicianNames);
+// Calling show_magicians() to see the modified list
+// show_magicians(magicianNames);
+/*--------------------------------2nd working strat---------------------*/
 function show_magicians(magicians) {
     magicians.forEach(magician => console.log(magician));
 }
+function make_great(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = "the Great " + magicians[i];
+    }
+}
 // Array of magician's names
 const magicianNames = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
-// Calling the function to show the magician's names
+// Adding "the Great" to each magician's name
+make_great(magicianNames);
+// Calling the function to show the modified magician's names
 show_magicians(magicianNames);
 export {};
