e201f1a5ffb775aa8660b6345d188530bfd607f7
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:15:37 +0500

    project-01
    Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”

---
+++ app.js
@@ -0,0 +1,18 @@
+// 2: Personal Message: Store a person’s name in a variable,
+//  and print a message to that person. Your message should be simple, such as, 
+// “Hello Eric, would you like to learn some Python today?”
+/*-----------------strat working----------*/
+/*----------------- 1st step working----------*/
+// let PersonName : string = '';
+// PersonName = prompt('What Is Your Name?') || '';
+// if(PersonName !== null && PersonName !== ''){
+//     alert(`Hello ${PersonName} would you like to learn some Python today?`)
+// } else{
+//     alert('Store a person’s name ! ')
+// }
+/*----------------- 2ns step working----------*/
+// Storing person's name in a variable
+const personName = "Eric";
+// Printing a personalized message
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
+export {};
