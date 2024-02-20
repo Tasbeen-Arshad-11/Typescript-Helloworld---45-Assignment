316a0ba4104390e4b7e1c34084246095500ac8f4
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:45:15 +0500

    project 35
    Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!

--- main.ts
+++ main.ts
@@ -1,33 +1,29 @@
-// 34: Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza 
-// names in a array, and then use a for loop to print the name of each pizza.
-// • Modify your for loop to print a sentence using the name of the pizza instead of
-//  printing just the name of the pizza. For each pizza you should have one line of output 
-//  containing a simple statement like I like pepperoni pizza.
+// 35: Animals: Think of at least three different animals that have a common characteristic.
 
-// • Add a line at the end of your program, outside the for loop, that states how much you 
-// like pizza. The output should consist of three or more lines about the kinds of pizza you 
-// like and then an additional sentence, such as I really love pizza!
+//  Store the names of these animals in a list, and then use a for loop to print out the name
+//   of each animal. • Modify your program to print a statement about each animal, such as 
+//   A dog would make a great pet. • Add a line at the end of your program stating what these 
+//   animals have in common. You could print a sentence such as Any of these animals would make
+//    a great pet!
 
-/*-------------------start working------------------*/
+/*-----------------------start working----------------------------*/
+// List containing different animals with a common characteristic
+const Animals: string[] = ["Dog", "Cat", "Rabbit"];
 
-// Array containing favorite pizza names
-const pizzas: string[] = ["California Pizza", "Mexico Pizza", "Detroit-style Pizza."];
-
-// Printing each pizza name
-console.log("Printing pizza names:");
-for (let pizza of pizzas) {
-    console.log(pizza);
+// Printing each animal name
+console.log("Printing animal names:");
+for (let Animal of Animals) {
+    console.log(Animal);
 }
 
-// Printing a sentence for each pizza
-console.log("\nPrinting sentences about each pizza:");
-for (let pizza of pizzas) {
-    console.log(`I like ${pizza} pizza.`);
+// Printing a statement about each animal
+console.log("\nPrinting statements about each animal:");
+for (let Animal of Animals) {
+    console.log(`A ${Animal.toLowerCase()} would make a great pet.`);
 }
 
-// Printing additional statement about how much I like pizza
-console.log("\nI really love pizza!");
-
+// Printing a statement about the common characteristic
+console.log("\nAny of these animals would make a great pet!");
 
 
 
