26ac0a2d4e49bbd718e22fde8a3fa9bc500eb268
Tasbeen-Arshad-11 <113518522+Tasbeen-Arshad-11@users.noreply.github.com>
2024-02-20 09:25:03 +0500

    project-12
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// 11: Names: Store the names of a few of your friends in a array called names.
-//  Print each person’s name by accessing each element in the list, one at a time.
-/*---------------------------------start working--------------*/
-/*-----------------------1st step---------------*/
-// declare a friends name 
-// let Names : string[] = ['Amna','Sara','Zehra','Minsa','Hira'];
-// for(let i=0; i<Names.length; i++){
-//     console.log(Names[i]);
+// 12: Greetings: Start with the array you used in Exercise 11, but instead of 
+// just printing each person’s name, print a message to them. The text of each message 
+// should be the same, but each message should be personalized with the person’s name.
+/*-------------------strat working-----------------*/
+/*---------------------1st step----------------*/
+// let Name : string[] = ['Amna','Sara','Zehra','Minsa','Hira'];
+// const message : string = 'She is a Genius Student:'
+// for(let i=0; i<Name.length; i++){
+//     console.log(message + Name[i]);
 // }
-/*-----------------------2nd step---------------*/
+/*---------------------2nd step----------------*/
 // Array containing the names of friends
-const Names = ['Amna', 'Sara', 'Zehra', 'Minsa', 'Hira'];
-// Printing each person's name one at a time
-Names.forEach(name => console.log(name));
+const Name = ['Amna', 'Sara', 'Zehra', 'Minsa', 'Hira'];
+// Printing a personalized message to each person
+Name.forEach(name => console.log(`Hello, ${name}! I hope you're having a great day.`));
 export {};
